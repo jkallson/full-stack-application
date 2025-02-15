129b7250f550cf00a4c47f918e6d106735a141f8
janar <janar.kallson@gmail.com>
2025-02-15 21:46:19 +0200

    fix: fix again

--- backend/src/main/java/com/assignment/backend/security/JwtUtil.java
+++ backend/src/main/java/com/assignment/backend/security/JwtUtil.java
@@ -43,7 +43,7 @@ public class JwtUtil {
                 .getSubject();
     }
 
-    private boolean isTokenExpired(String token) {
+    public boolean isTokenExpired(String token) {
         return Jwts.parser()
                 .verifyWith((SecretKey) key)
                 .build()
