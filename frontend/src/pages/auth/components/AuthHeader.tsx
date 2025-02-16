1faee098dab583b8aef37ca28d495c98ba0fbbc2
janar <janar.kallson@gmail.com>
2025-02-16 22:48:37 +0200

    feat: change title and alt text

--- frontend/src/pages/auth/components/AuthHeader.tsx
+++ frontend/src/pages/auth/components/AuthHeader.tsx
@@ -5,7 +5,7 @@ export function AuthHeader () {
         <div className="header">
             <div className="container">
                 <div className="header-logo-container">
-                    <img src="/company.svg" alt="EcoFit Logo"/>
+                    <img src="/company.svg" alt="GreenFlow Logo"/>
                 </div>
             </div>
         </div>
